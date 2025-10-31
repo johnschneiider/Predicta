2396e3b8b08175bac8b046e82c0d0584fdc4cdd7
John <johnsneiider@gmail.com>
2025-10-30 22:45:05 -0500

    fix(deploy): desactivar middleware personalizado ausente en VPS para permitir arranque de Gunicorn

--- betting_bot/settings.py
+++ betting_bot/settings.py
@@ -56,7 +56,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'betting_bot.middleware.AuthenticationMiddleware',
 ]
 
 ROOT_URLCONF = 'betting_bot.urls'
